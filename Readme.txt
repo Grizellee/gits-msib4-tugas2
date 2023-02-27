c470abf67abd65ffa7bca5ef1d5aa27389ef0ef8
Heru Setiawan Saputra <herumolen2017@gmail.com>
2023-02-27 09:10:04 +0700

    initial commit

---
+++ Readme.txt
@@ -0,0 +1,4 @@
+TUGAS 2 MSIB BATCH 4!
+
+Nama      : Heru Setiawan Saputra
+Instansi  : Universitas PGRI Semarang
---
+++ body.php
@@ -0,0 +1,353 @@
+<?php
+    echo"<!-- ======= Hero Section ======= -->
+    <section id='hero' class='d-flex flex-column justify-content-center align-items-center'>
+      <div class='hero-container' data-aos='fade-in'>
+        <h1>Heru Setiawan S.</h1>
+        <p>I'm <span class='typed' data-typed-items='Programer, Developer, Student'></span></p>
+      </div>
+    </section><!-- End Hero -->
+  
+    <main id='main'>
+  
+      <!-- ======= About Section ======= -->
+      <section id='about' class='about'>
+        <div class='container'>
+  
+          <div class='section-title'>
+            <h2>About</h2>
+            <p>Saya adalah mahasiswa dari Universitas PGRI Semarang angkatan 2020 Fakultas Teknik dan Informatika Program Studi Informatika</p>
+          </div>
+  
+          <div class='row'>
+            <div class='col-lg-4' data-aos='fade-right'>
+              <img src='assets/img/profile-img.jpg' class='img-fluid' alt=''>
+            </div>
+            <div class='col-lg-8 pt-4 pt-lg-0 content' data-aos='fade-left'>
+              <h3>Programmer Student.</h3>
+              <p class='fst-italic'>
+                Berikut merupakan profile saya :
+              </p>
+              <div class='row'>
+                <div class='col-lg-6'>
+                  <ul>
+                    <li><i class='bi bi-chevron-right'></i> <strong>Birthday:</strong> <span>30 Sept 2022</span></li>
+                    <li><i class='bi bi-chevron-right'></i> <strong>Website:</strong> <span>www.example.com</span></li>
+                    <li><i class='bi bi-chevron-right'></i> <strong>Phone:</strong> <span>082242325789</span></li>
+                    <li><i class='bi bi-chevron-right'></i> <strong>City:</strong> <span>Pemalang, Jateng</span></li>
+                  </ul>
+                
+                
+                  <ul>
+                    <li><i class='bi bi-chevron-right'></i> <strong>Age:</strong> <span>20</span></li>
+                    <li><i class='bi bi-chevron-right'></i> <strong>Degree:</strong> <span>Beginner</span></li>
+                    <li><i class='bi bi-chevron-right'></i> <strong>Email:</strong> <span>herumolen2017@gmail.com</span></li>
+                    <li><i class='bi bi-chevron-right'></i> <strong>Freelance:</strong> <span>Available</span></li>
+                  </ul>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+  
+        </div>
+      </section><!-- End About Section -->
+  
+      <!-- ======= Skills Section ======= -->
+      <section id='skills' class='skills section-bg'>
+        <div class='container'>
+  
+          <div class='section-title'>
+            <h2>Skills</h2>
+            </div>
+  
+          <div class='row skills-content'>
+  
+            <div class='col-lg-6' data-aos='fade-up'>
+  
+              <div class='progress'>
+                <span class='skill'>HTML <i class='val'>80%</i></span>
+                <div class='progress-bar-wrap'>
+                  <div class='progress-bar' role='progressbar' aria-valuenow='80' aria-valuemin='0' aria-valuemax='100'></div>
+                </div>
+              </div>
+  
+              <div class='progress'>
+                <span class='skill'>CSS <i class='val'>60%</i></span>
+                <div class='progress-bar-wrap'>
+                  <div class='progress-bar' role='progressbar' aria-valuenow='60' aria-valuemin='0' aria-valuemax='100'></div>
+                </div>
+              </div>
+  
+              <div class='progress'>
+                <span class='skill'>JavaScript <i class='val'>50%</i></span>
+                <div class='progress-bar-wrap'>
+                  <div class='progress-bar' role='progressbar' aria-valuenow='50' aria-valuemin='0' aria-valuemax='100'></div>
+                </div>
+              </div>
+  
+            </div>
+  
+            <div class='col-lg-6' data-aos='fade-up' data-aos-delay='100'>
+  
+              <div class='progress'>
+                <span class='skill'>PHP <i class='val'>50%</i></span>
+                <div class='progress-bar-wrap'>
+                  <div class='progress-bar' role='progressbar' aria-valuenow='50' aria-valuemin='0' aria-valuemax='100'></div>
+                </div>
+              </div>
+  
+              <div class='progress'>
+                <span class='skill'>WordPress/CMS <i class='val'>80%</i></span>
+                <div class='progress-bar-wrap'>
+                  <div class='progress-bar' role='progressbar' aria-valuenow='80' aria-valuemin='0' aria-valuemax='100'></div>
+                </div>
+              </div>
+  
+              <div class='progress'>
+                <span class='skill'>Photoshop <i class='val'>65%</i></span>
+                <div class='progress-bar-wrap'>
+                  <div class='progress-bar' role='progressbar' aria-valuenow='65' aria-valuemin='0' aria-valuemax='100'></div>
+                </div>
+              </div>
+  
+            </div>
+  
+          </div>
+  
+        </div>
+      </section><!-- End Skills Section -->
+  
+      <!-- ======= Resume Section ======= -->
+      <section id='resume' class='resume'>
+        <div class='container'>
+  
+          <div class='section-title'>
+            <h2>Resume</h2>
+            <p>Sekarang saya sedang menempuh ilmu di salah satu Universitas Swasta di Semarang yaitu di Universitas PGRI Semarang.</p>
+            </div>
+  
+          <div class='row'>
+            <div class='col-lg-6' data-aos='fade-up'>
+              <h3 class='resume-title'>Education</h3>
+              <div class='resume-item'>
+                <h4>Sekolah Dasar 04 Kejene</h4>
+                <h5>2008 - 2014</h5>
+                <p>Saya lulus SD dengan mendapatkan NEM 23</p>
+              </div>
+              <div class='resume-item'>
+                <h4>Sekolah Menengah Pertama</h4>
+                <h5>2014 - 2016</h5>
+                <p>Saya lulus SMP dengan mendapatkan NEM 23</p>
+              </div>
+              <div class='resume-item'>
+                <h4>Sekolah Menengah Akhir</h4>
+                <h5>2016 - 2019</h5>
+                <p>Saya lulus SMA dengan nilai : </p>
+                <ul>
+                  <li>Nilai rata-rata Raport : 81</li>
+                  <li>Nilai Ujian Sekolah : 84</li>
+                  <li>Nilai rata-rata : 83</li>
+                  <li>Nilai UTBK : 515</li>
+                </ul>
+              </div>
+              <div class='resume-item'>
+                <h4>Universitas PGRI Semarang</h4>
+                <h5>2020 - now</h5>
+                <p>IPK sementara 3,74 dengan SKS 86</p>
+              </div>
+            </div>
+            <div class='col-lg-6' data-aos='fade-up' data-aos-delay='100'>
+              <h3 class='resume-title'>Professional Experience</h3>
+              <div class='resume-item'>
+                <h4>Studi Independent CV. FUN Technology</h4>
+                <h5>3 Bulan</h5>
+                <p><em>Online</em></p>
+                <ul>
+                  <li>Membuat desain menggunakan Adobe Animate dan Blender</li>
+                  <li>Mengerjakan semua project yang telah ditentukan dalam waktu tertentu dengan tepat waktu</li>
+                  <li>Selalu bertukar informasi dengan anggota lain agar mendapatkan hasil yang maksimal</li>
+                </ul>
+              </div>
+              <div class='resume-item'>
+                <h4>Robotika UPGRIS</h4>
+                <h5>2022 - now</h5>
+                <p><em>Ukm Upgris</em></p>
+                <ul>
+                  <li>Membuat pintu otomatis berbasis android (Arduino & MIT APP)</li>
+                </ul>
+              </div>
+              <div class='resume-item'>
+                <h4>Magang di PT. Laksana</h4>
+                <h5>3 bulan</h5>
+                <p><em>Praktik Kerja Lapangan (Program MBKM)</em></p>
+                <ul>
+                  <li>Melakukan pekerjaan seperti karyawan disana</li>
+                  
+                </ul>
+              </div>
+            </div>
+          </div>
+  
+        </div>
+      </section><!-- End Resume Section -->
+  
+      <!-- ======= Portfolio Section ======= -->
+      <section id='portfolio' class='portfolio section-bg'>
+        <div class='container'>
+  
+          <div class='section-title'>
+            <h2>Portfolio</h2>
+            <p>Berikut hasil yang pernah saya lakukan di tempat Magang dan Studi Independen :</p>
+          </div>
+  
+          <div class='row' data-aos='fade-up'>
+            <div class='col-lg-12 d-flex justify-content-center'>
+              <ul id='portfolio-flters'>
+                <li data-filter='*' class='filter-active'>All</li>
+                <li data-filter='.filter-app'>Design</li>
+                <li data-filter='.filter-card'>Sertificate</li>
+              </ul>
+            </div>
+          </div>
+  
+          <div class='row portfolio-container' data-aos='fade-up' data-aos-delay='100'>
+  
+            <div class='col-lg-4 col-md-6 portfolio-item filter-app'>
+              <div class='portfolio-wrap'>
+                <img src='assets/img/portfolio/portfolio-1.jpg' class='img-fluid' alt=''>
+                <div class='portfolio-links'>
+                  <a href='assets/img/portfolio/portfolio-1.jpg' data-gallery='portfolioGallery' class='portfolio-lightbox' title='Design 1'><i class='bx bx-plus'></i></a>
+                  <a href='' title='More Details'><i class='bx bx-link'></i></a>
+                </div>
+              </div>
+            </div>
+  
+            <div class='col-lg-4 col-md-6 portfolio-item filter-app'>
+              <div class='portfolio-wrap'>
+                <img src='assets/img/portfolio/portfolio-2.jpg' class='img-fluid' alt=''>
+                <div class='portfolio-links'>
+                  <a href='assets/img/portfolio/portfolio-2.jpg' data-gallery='portfolioGallery' class='portfolio-lightbox' title='Design 2'><i class='bx bx-plus'></i></a>
+                  <a href='' title='More Details'><i class='bx bx-link'></i></a>
+                </div>
+              </div>
+            </div>
+  
+            <div class='col-lg-4 col-md-6 portfolio-item filter-app'>
+              <div class='portfolio-wrap'>
+                <img src='assets/img/portfolio/portfolio-3.png' class='img-fluid' alt=''>
+                <div class='portfolio-links'>
+                  <a href='assets/img/portfolio/portfolio-3.png' data-gallery='portfolioGallery' class='portfolio-lightbox' title='Design 3'><i class='bx bx-plus'></i></a>
+                  <a href='' title='More Details'><i class='bx bx-link'></i></a>
+                </div>
+              </div>
+            </div>
+  
+            <div class='col-lg-4 col-md-6 portfolio-item filter-card'>
+              <div class='portfolio-wrap'>
+                <img src='assets/img/portfolio/portfolio-4.jpeg' class='img-fluid' alt=''>
+                <div class='portfolio-links'>
+                  <a href='assets/img/portfolio/portfolio-4.jpeg' data-gallery='portfolioGallery' class='portfolio-lightbox' title='Sertificate 1'><i class='bx bx-plus'></i></a>
+                  <a href='' title='More Details'><i class='bx bx-link'></i></a>
+                </div>
+              </div>
+            </div>
+  
+            <div class='col-lg-4 col-md-6 portfolio-item filter-card'>
+              <div class='portfolio-wrap'>
+                <img src='assets/img/portfolio/portfolio-5.jpeg' class='img-fluid' alt=''>
+                <div class='portfolio-links'>
+                  <a href='assets/img/portfolio/portfolio-5.jpeg' data-gallery='portfolioGallery' class='portfolio-lightbox' title='Sertificate 2'><i class='bx bx-plus'></i></a>
+                  <a href='' title='More Details'><i class='bx bx-link'></i></a>
+                </div>
+              </div>
+            </div>
+  
+            <div class='col-lg-4 col-md-6 portfolio-item filter-card'>
+              <div class='portfolio-wrap'>
+                <img src='assets/img/portfolio/portfolio-6.jpeg' class='img-fluid' alt=''>
+                <div class='portfolio-links'>
+                  <a href='assets/img/portfolio/portfolio-6.jpeg' data-gallery='portfolioGallery' class='portfolio-lightbox' title='Sertificate 3'><i class='bx bx-plus'></i></a>
+                  <a href='' title='More Details'><i class='bx bx-link'></i></a>
+                </div>
+              </div>
+            </div>
+  
+            
+  
+          </div>
+  
+        </div>
+      </section><!-- End Portfolio Section -->
+  
+      <!-- ======= Contact Section ======= -->
+      <section id='contact' class='contact'>
+        <div class='container'>
+  
+          <div class='section-title'>
+            <h2>Contact</h2>
+            <p>Jika anda mempunyai pertanyaan, anda bisa menghubungi kami melewati platform berikut ini:</p>
+          </div>
+  
+          <div class='row' data-aos='fade-in'>
+  
+            <div class='col-lg-5 d-flex align-items-stretch'>
+              <div class='info'>
+                <div class='address'>
+                  <i class='bi bi-geo-alt'></i>
+                  <h4>Location:</h4>
+                  <p>jl. Raya Gunung Gajah Kejene, Pemalang 52353</p>
+                </div>
+  
+                <div class='email'>
+                  <i class='bi bi-envelope'></i>
+                  <h4>Email:</h4>
+                  <p>herumolen2017@gmail.com</p>
+                </div>
+  
+                <div class='phone'>
+                  <i class='bi bi-phone'></i>
+                  <h4>Call:</h4>
+                  <p>+62 8224 2325 789</p>
+                </div>
+  
+                <iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31678.795734160434!2d109.2919573976108!3d-7.026974125150689!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6fe9de513f6fd1%3A0xea80a46d398e4876!2sKejene%2C%20Kec.%20Randudongkal%2C%20Kabupaten%20Pemalang%2C%20Jawa%20Tengah!5e0!3m2!1sid!2sid!4v1677331959346!5m2!1sid!2sid' width='600' height='450' style='border:0;' allowfullscreen='' loading='lazy' referrerpolicy='no-referrer-when-downgrade'></iframe>
+              </div>
+  
+            </div>
+  
+            <div class='col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch'>
+              <form action='forms/contact.php' method='post' role='form' class='php-email-form'>
+                <div class='row'>
+                  <div class='form-group col-md-6'>
+                    <label for='name'>Your Name</label>
+                    <input type='text' name='name' class='form-control' id='name' required>
+                  </div>
+                  <div class='form-group col-md-6'>
+                    <label for='name'>Your Email</label>
+                    <input type='email' class='form-control' name='email' id='email' required>
+                  </div>
+                </div>
+                <div class='form-group'>
+                  <label for='name'>Subject</label>
+                  <input type='text' class='form-control' name='subject' id='subject' required>
+                </div>
+                <div class='form-group'>
+                  <label for='name'>Message</label>
+                  <textarea class='form-control' name='message' rows='10' required></textarea>
+                </div>
+                <div class='my-3'>
+                  <div class='loading'>Loading</div>
+                  <div class='error-message'></div>
+                  <div class='sent-message'>Your message has been sent. Thank you!</div>
+                </div>
+                <div class='text-center'><button type='submit'>Send Message</button></div>
+              </form>
+            </div>
+  
+          </div>
+  
+        </div>
+      </section><!-- End Contact Section -->
+  
+    </main><!-- End #main -->";
+?>
